e57dd38f19b9b5ffc60255906c70d3998ef418aa
Igor Lima <igorlimadrum@gmail.com>
2024-01-30 16:57:17 -0300

    implement Footer component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import "./App.css";
 function App() {
       <Navbar />
+      <Footer />
 }
 
 export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import "./index.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">Copyright © All Right Reserved</div>
+    </footer>
+  );
+};
+
+export default Footer;
