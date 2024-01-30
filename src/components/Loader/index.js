5cbb5b0ac12420ff6fe1c1ea3cbdb6d1186baf3f
Igor Lima <igorlimadrum@gmail.com>
2024-01-30 17:03:29 -0300

    implement Loader component

---
+++ src/components/Loader/index.js
@@ -0,0 +1,15 @@
+import "./index.css";
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <div className="loading">
+        <div className="spinner-border aloader" role="status">
+          <span className="sr-only"></span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader();
